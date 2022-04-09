d82a63a58c743a2d59a35f3bf2db520a02651732
PJH <carpediem.mementomori.1984@gmail.com>
2022-04-09 22:26:27 +0900

    Add: generate Board

--- src/pages/Board.tsx
+++ src/pages/Board.tsx
@@ -1,18 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { CustomLevelType } from './Game/Game';
 
-type Props = {};
+enum CellStatus {
+  Opened = 'OPENED',
+  Closed = 'CLOSED',
+  Bomb = 'BOMB',
+}
 
-const Board = (props: Props) => {
+type BoardType = string[][];
+
+type Props = {
+  cellInfoNumbers: CustomLevelType;
+};
+
+const Board = ({ cellInfoNumbers }: Props) => {
+  const { row, column, bomb } = cellInfoNumbers;
+  const [boradArray, setBoardArray] = useState<BoardType>();
+
+  useEffect(() => {
+    console.log('[Board/useEffect] cellInfoNumbers', { row, column, bomb });
+    generateBoard(column, row);
+  }, [bomb, column, row]);
+
+  //1. generate base board array at start
+  const generateBoard = (row: number, column: number) => {
+    const resultArray: BoardType = [];
+
+    for (let i = 0; i < row; i++) {
+      resultArray.push([]);
+      for (let j = 0; j < column; j++) {
+        resultArray[i][j] = CellStatus.Opened;
+      }
+    }
+
+    setBoardArray(resultArray);
+  };
+
   return (
     <BoardWrapper>
-      <BoardBox>Board</BoardBox>
+      <BoardBox>
+        {boradArray?.map((row, index) => (
+          <BoardTableRow key={index}>
+            {row.map((item, index) => {
+              return <BoardTableData />;
+            })}
+          </BoardTableRow>
+        ))}
+      </BoardBox>
     </BoardWrapper>
   );
 };
 
 export default Board;
 
-const BoardWrapper = styled.table``;
+const BoardWrapper = styled.table`
+  padding: 5px;
+`;
 
 const BoardBox = styled.tbody``;
+
+const BoardTableRow = styled.tr``;
+
+const BoardTableData = styled.td`
+  width: 1rem;
+  height: 1rem;
+  background-color: ${(props) => props.theme.colors.main};
+`;
